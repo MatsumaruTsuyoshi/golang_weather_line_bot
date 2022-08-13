87b7c5a82c4d65b4ae80d8d834702310b7e72456
MatsumaruTsuyoshi <strivesdfpx379@gmail.com>
2022-08-13 13:54:22 +0900

    create mian

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/MatsumaruTsuyoshi/golang_weather_line_bot
+
+go 1.19
+
+require github.com/line/line-bot-sdk-go v7.8.0+incompatible // indirect
---
+++ main.go
@@ -0,0 +1,27 @@
+func main() {
+
+	// LINE Botクライアント生成する
+	// BOT にはチャネルシークレットとチャネルトークンを環境変数から読み込み引数に渡す
+	bot, err := linebot.New(
+		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+	)
+
+	// エラーに値があればログに出力し終了する
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// weatherパッケージパッケージから天気情報の文字列をを取得する
+	result, err := weather.GetWeather()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	message := linebot.NewTextMessage(result)
+
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
+}
